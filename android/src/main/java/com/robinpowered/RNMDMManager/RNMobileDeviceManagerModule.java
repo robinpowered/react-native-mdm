e6da923aa92b69ff510ebb722208b3aef4c12deb
Ying Hang Eng <eyinghang@gmail.com>
2017-10-06 13:19:34 +0800

    fix indents

--- android/src/main/java/com/robinpowered/RNMDMManager/RNMobileDeviceManagerModule.java
+++ android/src/main/java/com/robinpowered/RNMDMManager/RNMobileDeviceManagerModule.java
@@ -16,67 +16,67 @@ import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class RNMobileDeviceManagerModule extends ReactContextBaseJavaModule {
 
-	private final ReactApplicationContext reactContext;
+    private final ReactApplicationContext reactContext;
 
-	// RM - For MDM
-	RestrictionsManager restrictionsManager;
-  Bundle appRestrictions;
-  IntentFilter restrictionFilter;
-  BroadcastReceiver restrictionReceiver;
+    // RM - For MDM
+    RestrictionsManager restrictionsManager;
+    Bundle appRestrictions;
+    IntentFilter restrictionFilter;
+    BroadcastReceiver restrictionReceiver;
 
-	public RNMobileDeviceManagerModule(ReactApplicationContext reactContext) {
-		super(reactContext);
-    this.reactContext = reactContext;
-    final ReactApplicationContext thisContext = reactContext;
+    public RNMobileDeviceManagerModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+        final ReactApplicationContext thisContext = reactContext;
 
-    restrictionsManager = (RestrictionsManager) reactContext.getSystemService(Context.RESTRICTIONS_SERVICE);
+        restrictionsManager = (RestrictionsManager) reactContext.getSystemService(Context.RESTRICTIONS_SERVICE);
 
-    restrictionFilter = new IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED);
-    restrictionReceiver = new BroadcastReceiver() {
-      @Override
-      public void onReceive(Context context, Intent intent) {
-        if (thisContext.hasActiveCatalystInstance()) {
-          thisContext
-            .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
-            .emit("userDefaultsDidChange", restrictionsManager.getApplicationRestrictions());
+        restrictionFilter = new IntentFilter(Intent.ACTION_APPLICATION_RESTRICTIONS_CHANGED);
+        restrictionReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (thisContext.hasActiveCatalystInstance()) {
+                    thisContext
+                        .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                        .emit("userDefaultsDidChange", restrictionsManager.getApplicationRestrictions());
+                }
+            }
+        };
+    }
+
+    @Override
+    public String getName() {
+        return "MobileDeviceManager";
+    }
+
+    private boolean isMDMSupported() {
+        // Instantiating the restriction manager
+        appRestrictions = restrictionsManager.getApplicationRestrictions();
+        boolean isSupported = appRestrictions.size() > 0;
+        return isSupported;
+    }
+
+    private Bundle getConfigs() {
+        // Instantiating the restriction manager
+        appRestrictions = restrictionsManager.getApplicationRestrictions();
+        return appRestrictions;
+    }
+
+    @ReactMethod
+    public void isSupported(Callback successCallback) {
+        if (isMDMSupported()) {
+            successCallback.invoke(null, true);
+        } else {
+            successCallback.invoke(true, null);
         }
-      }
-    };
-	}
+    }
 
-	@Override
-	public String getName() {
-	  return "MobileDeviceManager";
-	}
-
-	private boolean isMDMSupported() {
-		// Instantiating the restriction manager
-		appRestrictions = restrictionsManager.getApplicationRestrictions();
-		boolean isSupported = appRestrictions.size() > 0;
-		return isSupported;
-	}
-
-	private Bundle getConfigs() {
-		// Instantiating the restriction manager
-		appRestrictions = restrictionsManager.getApplicationRestrictions();
-		return appRestrictions;
-	}
-
-	@ReactMethod
-	public void isSupported(Callback successCallback) {
-		if (isMDMSupported()) {
-			successCallback.invoke(null, true);
-		} else {
-			successCallback.invoke(true, null);
-		}
-	}
-
-	@ReactMethod
-	public void getConfiguration(Callback successCallback) {
-		if (isMDMSupported()) {
-			successCallback.invoke(null, getConfigs());
-		} else {
-			successCallback.invoke(true, null);
-		}
-	}
+    @ReactMethod
+    public void getConfiguration(Callback successCallback) {
+        if (isMDMSupported()) {
+            successCallback.invoke(null, getConfigs());
+        } else {
+            successCallback.invoke(true, null);
+        }
+    }
 }
